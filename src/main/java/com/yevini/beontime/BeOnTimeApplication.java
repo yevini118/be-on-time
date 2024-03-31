26f718917666deaff1cd3013dbb1a0c2557e9181
Yevin Lee <yevini12@gmail.com>
2024-03-31 18:13:54 +0900

    feat : 프로젝트 기본 세팅

---
+++ src/main/java/com/yevini/beontime/BeOnTimeApplication.java
@@ -0,0 +1,13 @@
+package com.yevini.beontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeOnTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeOnTimeApplication.class, args);
+	}
+
+}
